734e9f96c4e021053291bf87029f72bda458ac1d
donatas <petrikauskas.donatas@gmail.com>
2021-12-20 20:34:27 +0200

    Bootstrap responsive layout implementation, admin page loading bug fix, footer background color change, some adjustments

--- src/components/TestimonialsSection.vue
+++ src/components/TestimonialsSection.vue
@@ -8,22 +8,22 @@
         <div class="col-lg-4">
           <div class="testimonial-item mx-auto mb-5 mb-lg-0">
             <img class="img-fluid rounded-circle mb-3" src="/img/woman.png" alt="" />
-            <h5>Margaret E.</h5>
-            <p class="font-weight-light mb-0">"This is fantastic! Thanks so much guys!"</p>
+            <h5>Rūta</h5>
+            <p class="font-weight-light mb-0">"This is the best service, that ever existed."</p>
           </div>
         </div>
         <div class="col-lg-4">
           <div class="testimonial-item mx-auto mb-5 mb-lg-0">
             <img class="img-fluid rounded-circle mb-3" src="/img/Male-Avatar.svg" alt="" />
-            <h5>Fred S.</h5>
-            <p class="font-weight-light mb-0">"Bootstrap is amazing. I've been using it to create lots of super nice landing pages."</p>
+            <h5>Jurgis</h5>
+            <p class="font-weight-light mb-0">"Using this service I've made posts so much easier. Thanks guys."</p>
           </div>
         </div>
         <div class="col-lg-4">
           <div class="testimonial-item mx-auto mb-5 mb-lg-0">
             <img class="img-fluid rounded-circle mb-3" src="/img/Cartoon-Woman-Avatar.svg" alt="" />
-            <h5>Sarah W.</h5>
-            <p class="font-weight-light mb-0">"Thanks so much for making these free resources available to us!"</p>
+            <h5>Jurgita</h5>
+            <p class="font-weight-light mb-0">"Thank you team, for this system"</p>
           </div>
         </div>
       </div>
